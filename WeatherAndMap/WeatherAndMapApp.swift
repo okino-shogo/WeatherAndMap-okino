1d56866d7a184c99b58540b5e866ca0db8e13435
okino shogo <okisho0106@gmail.com>
2025-02-15 16:30:02 +0900

    Initial Commit

---
+++ WeatherAndMap/WeatherAndMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAndMapApp.swift
+//  WeatherAndMap
+//
+//  Created by 沖野匠吾 on 2025/02/15.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAndMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
